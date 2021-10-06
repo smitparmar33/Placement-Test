3c146b59ba7f3cc20607ae3de6646a2608a74a4e
Dhiraj Beri <46566839+dhirajberi@users.noreply.github.com>
2021-10-06 15:01:30 +0530

    admin customization, quiz page new line issue solved, minor css bugs fixed

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 
+# admin customizations
+admin.site.site_header = "Welcome to Yudiz Placement Drive"
+admin.site.site_title = "Yudiz Placement Drive"
+admin.site.index_title = "Yudiz Placement Portal"
 
 urlpatterns=[
     path('register',views.register,name='register'),
